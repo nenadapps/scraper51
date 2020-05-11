9ff518f70919309ea2579a4b1b170ab104498111
Nenad <nenadoks@yahoo.com>
2020-05-11 15:05:01 +0200

    Solution implemented for large-lots

--- script.py
+++ script.py
@@ -18,7 +18,39 @@ def get_html(url):
     
     return html_content
 
-def get_details(html, subcategory, stamp_url):
+def get_images(url):
+    
+    images = []
+    
+    try:
+        html = get_html(url)
+        if html.select('.all_images_link a'):
+            all_images_href = html.select('.all_images_link a')[0].get('href')
+            if base_url not in all_images_href:  
+                all_images_url = base_url + all_images_href
+                all_images_html = get_html(all_images_url)
+            else:
+                all_images_url = all_images_href
+                
+            all_images_html = get_html(all_images_url)
+            
+            for img_item in all_images_html.select('p img'):
+                image_src = img_item.get('src')
+                image = base_url + image_src
+                if not image in images:
+                    images.append(image)
+        else:
+            image_src = html.select('.prod_detail img')[0].get('src')
+            image = base_url + image_src
+            images.append(image)
+
+    except Exception as e: 
+        print(e)
+        pass
+    
+    return images
+
+def get_details(html, category, subcategory, stamp_url):
     
     stamp = {}
     
@@ -42,7 +74,7 @@ def get_details(html, subcategory, stamp_url):
     except:
         stamp['price'] = None  
     
-    stamp['category'] = 'country-stamps'
+    stamp['category'] = category
     stamp['subcategory'] = subcategory
     
     stamp['currency'] = 'USD'
@@ -50,11 +82,16 @@ def get_details(html, subcategory, stamp_url):
     # image_urls should be a list
     images = []                    
     try:
-        image_items = html.select('img')
-        for image_item in image_items:
-            img = base_url + image_item.get('src')
-            if img not in images:
-                images.append(img)
+        if category == 'large-lots':
+            image_page_href = html.select('a')[0].get('href')
+            image_page_url = base_url + image_page_href
+            images = get_images(image_page_url)
+        else:
+            image_items = html.select('img')
+            for image_item in image_items:
+                img = base_url + image_item.get('src')
+                if img not in images:
+                    images.append(img)
     except:
         pass
     
@@ -93,14 +130,20 @@ def get_page_items(url):
         for next_item in html.select('form a'):
             next_text = next_item.get_text().strip()
             if next_text == 'Next':
-                 next_url = base_url + next_item.get('href')
-                 break
+                next_url = base_url + next_item.get('href')
+                break
     except:
         pass
    
     shuffle(list(set(items)))
     
     return items, next_url
+
+def get_item_details(page_url,category, subcategory, stamp_url):
+      while(page_url):
+        page_items, page_url = get_page_items(page_url)
+        for page_item in page_items:
+            stamp = get_details(page_item, category, subcategory, stamp_url)
 
 def get_subcategories():
     
@@ -126,10 +169,22 @@ def get_subcategories():
     
     return items
 
-subcategories = get_subcategories()   
-for subcategory in subcategories:
-    page_url = subcategories[subcategory]
-    while(page_url):
-        page_items, page_url = get_page_items(page_url)
-        for page_item in page_items:
-            stamp = get_details(page_item, subcategory, subcategories[subcategory]) 
+categories = [
+"country-stamps",
+"large-lots"
+    ]
+    
+for category in categories:
+    print(category)   
+
+selection = input('Choose category: ')
+
+if selection == 'country-stamps':
+    subcategories = get_subcategories()   
+    for subcategory in subcategories:
+        page_url = subcategories[subcategory]
+        get_item_details(page_url, 'country-stamps' , subcategory, subcategories[subcategory])
+elif selection == 'large-lots':
+    input_url = 'https://www.herrickstamp.com/large-lots'
+    get_item_details(input_url,'large-lots' ,'', input_url)       
+    
